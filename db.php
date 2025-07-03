fa4770cfeef832ff874b0715ed3b115b33879675
alexandre-mateos <mateosalexandre95@gmail.com>
2025-07-03 09:20:40 +0200

    creation de l'index et du formulaire d'ajout

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+
+//Connection to the database : examen_php_maison in localhost
+$pdo = new PDO(
+      'mysql:host=localhost;dbname=examen_php_maison;charset=utf8',
+      'root',
+      ''
+);
+
+//var_dump($pdo);
+
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require "db.php";
+
+//Prepared SQL query to retrieve all the data from the "maison" table
+$sql = "SELECT * FROM maison";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$houses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($houses);
+?>
+<?php include "header.php" ?>
+<main>
+      <table>
+            <tr>
+                  <th>Nom</th>
+                  <th>Adresse</th>
+                  <th>Nombre de pièces</th>
+                  <th>Prix</th>
+            </tr>
+            //Generate all the the table lines with a loop, using data from table "maison" ($houses)
+            <?php foreach ($houses as $house) : ?>
+                  <tr>
+                        <td><?php echo $house["nom"] ?></td>
+                        <td><?php echo $house["adresse"] ?></td>
+                        <td><?php echo $house["nb_pieces"] ?></td>
+                        <td><?php echo $house["prix"] ?> €</td>
+                  </tr>
+            <?php endforeach; ?>
+      </table>
+</main>
+<?php include "footer.php" ?>
+
+
